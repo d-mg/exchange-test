f21313bf0dab02cf28c9d9e9bf2f8ec8d36de0e5
Dries Goethuys <dries.goethuys@gmail.com>
2023-06-03 15:14:28 -0400

    Change save function

--- src/providers/postgres/index.js
+++ src/providers/postgres/index.js
@@ -6,78 +6,35 @@ import {
     COUNTRY
 } from "../../constants.js";
 
-export async function save({ entity, data, parent, }) {
-    // TODO: remove logging response
-    let response;
-    switch (entity) {
-    case EXCHANGE_OFFICE:
-        response = await saveExchangeOffice(data);
-        break;
-    case EXCHANGE:
-        response = await saveExchange(data, parent);
-        break;
-    case RATE:
-        response = await saveRate(data, parent);
-        break;
-    case COUNTRY:
-        response = await saveCountry(data);
-        break;
-    default:
-        throw new Error(`unknown entity`);
-    }
-    console.log(response.rows[0]);
-}
-
-export async function destroy() {
-    getClient().end();
-}
-
-async function saveExchangeOffice(data) {
-    return getClient().query(
-        `INSERT INTO exchange_office(
-            "id",
-            "name",
-            "country"
-        ) VALUES($1, $2, $3) RETURNING *`,
-        [
+const saveData = {
+    [EXCHANGE_OFFICE]: {
+        table: `exchange_office`,
+        props: `"id", "name", "country"`,
+        placeholders: `$1, $2, $3`,
+        values: ({ data, }) => [
             data.id,
             data.name,
             data.country,
-        ]);
-}
-
-async function saveExchange(data, parent) {
-    return getClient().query(
-        `INSERT INTO exchange(
-            "exchange_office",
-            "from",
-            "to",
-            "ask",
-            "bid",
-            "date"
-        ) VALUES($1, $2, $3, $4, $5, $6) RETURNING *`,
-        [
+        ],
+    },
+    [EXCHANGE]: {
+        table: `exchange`,
+        props: `"exchange_office", "from", "to", "ask", "bid", "date"`,
+        placeholders: `$1, $2, $3, $4, $5, $6`,
+        values: ({ data, parent, }) => [
             parent.data.id,
             data.from,
             data.to,
             data.ask,
             data.bid,
             data.date,
-        ]);
-}
-
-async function saveRate(data, parent) {
-    return getClient().query(
-        `INSERT INTO rate(
-            "exchange_office",
-            "from",
-            "to",
-            "in",
-            "out",
-            "reserve",
-            "date"
-        ) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *`,
-        [
+        ],
+    },
+    [RATE]: {
+        table: `rate`,
+        props: `"exchange_office", "from", "to", "in", "out", "reserve", "date"`,
+        placeholders: `$1, $2, $3, $4, $5, $6, $7`,
+        values: ({ data, parent, }) => [
             parent.data.id,
             data.from,
             data.to,
@@ -85,17 +42,29 @@ async function saveRate(data, parent) {
             data.out,
             data.reserve,
             data.date,
-        ]);
-}
-
-async function saveCountry(data) {
-    return getClient().query(
-        `INSERT INTO country(
-            "code",
-            "name"
-        ) VALUES($1, $2) RETURNING *`,
-        [
+        ],
+    },
+    [COUNTRY]: {
+        table: `country`,
+        props: `"code", "name"`,
+        placeholders: `$1, $2`,
+        values: ({ data, }) => [
             data.code,
             data.name,
-        ]);
+        ],
+    },
+};
+
+export async function save({ entity, data, parent, }) {
+    const { table, props, placeholders, values, } = saveData[entity];
+    const response = await getClient().query(
+        `INSERT INTO ${table}(${props}) VALUES(${placeholders}) RETURNING *`,
+        values({ data, parent, })
+    );
+    // TODO: remove logging
+    console.log(response.rows[0]);
+}
+
+export async function destroy() {
+    getClient().end();
 }
